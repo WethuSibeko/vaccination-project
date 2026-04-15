8026ad4afd03d3b1ce31632f7c38bd9349fd1899
christineomotosho <omotoshochristine4@gmail.com>
2026-04-15 11:55:55 +0200

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 public class MainApp{
     public static void main (String[]args){
-    Scanner in= new Scanner(System.in);
+    Scanner in = new Scanner(System.in);
     int num;
     do{
          System.out.println("1)Register Patients 2)View Patients 3)add Vaccination Records 4)view Vaccination Records 0)Exit");
     num= in.nextInt();
     switch (num) {
         case 1:
-            //call method to register
+            //call method to register patients
             break;
         case 2:
             // call method to display patients
@@ -22,10 +22,10 @@ public class MainApp{
             //call method to view vaccination records 
             break;
          case 0:
-            System.out.println("Bye");
+            System.out.println("Goodbye");
             break;
         default:
-            System.out.println("Please enter one of the number options");
+            System.out.println("Error: Please enter one of the number options");
       }
     } while(num !=0);
 }
