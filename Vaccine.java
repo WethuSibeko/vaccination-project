3fdcde216df00a749475e1e09686f2e71cb740a8
christineomotosho <omotoshochristine4@gmail.com>
2026-04-15 11:51:07 +0200

    Update Vaccine.java

--- Vaccine.java
+++ Vaccine.java
@@ -4,4 +4,9 @@ public class Vaccine{
  private String name;
  private double doseRequired;
  private String vaccineID;
+ 
+ public Vaccine(String name, String vaccineID, double doseRequired){
+  this.name=name;
+  this.vaccineID=vaccineID;
+  this.doseRequired = doseRequired;
 }
