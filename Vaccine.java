6948bf043fc58ac7784d3e6662bbcc7ae2e1637c
christineomotosho <omotoshochristine4@gmail.com>
2026-04-21 18:16:13 +0200

    Update Vaccine.java

--- Vaccine.java
+++ Vaccine.java
@@ -4,9 +4,29 @@ public class Vaccine{
  private String name;
  private double doseRequired;
  private String vaccineID;
+ private int doseIntervals;
  
- public Vaccine(String name, String vaccineID, double doseRequired){
-  this.name = name;
+ public Vaccine(String Vname, String vaccineID, double doseRequired, int doseIntervals){
+  this.Vname = Vname;
   this.vaccineID = vaccineID;
   this.doseRequired = doseRequired;
+  this.doseIntervals = doseIntervals;
 }
+public String getvaccineID(){
+    return vaccineID;
+}
+public String getVname(){
+    return Vname;
+}
+public double getDoseRequired(){
+    return doseRequired;
+}
+public int getDoseIntervals(){
+    return doseIntervals;
+}
+public void Vdetails(){
+    System.out.println(vaccineID + " " + Vname + "Dose required: " + doseRequired + " Booster every " + doseIntervals + "days");
+}
+
+}
+
