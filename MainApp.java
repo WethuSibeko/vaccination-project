21cab5e64042bd2bfeb28351ace5bc1c9ea57cde
NelileMkhabela <rebeccamk989@gmail.com>
2026-04-22 17:13:11 +0200

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -11,15 +11,19 @@ public class MainApp{
     switch (num) {
         case 1:
             //call method to register patients
+            addPatient();
             break;
         case 2:
             // call method to display patients
+            viewPatients();
             break;
          case 3:
             //call method to add vaccination records
+            addRecord();
             break;
          case 4:
             //call method to view vaccination records 
+            viewRecord();
             break;
          case 0:
             System.out.println("Goodbye");
@@ -29,4 +33,50 @@ public class MainApp{
       }
     } while(num !=0);
 }
+    public static void addPatient(){
+        System.out.print("Enter ID");
+        String id = scanner.nextLine();
+
+        System.out.print("Enter Name");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter Age");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        Patient p = new Patient(id, name, age);
+        DataStore.patients.add(p);
+
+        System.out.println("Patient added!");
+    }
+
+    public static void addRecord(){
+        System.out.print("Enter Patient ID: ")
+            String patientId= scanner.nextLine();
+
+        System.out.print("Enter Vaccine Name");
+        String vaccineName= scanner.nextLine();
+
+        System.out.print("Enter Date:"):
+            String date = scanner.nextLine();
+
+        VaccinationRecord record = new VaccinationRecord(patinteId, vaccineName, date);
+        DataStore.records.add(record);
+
+        System.out.println("Record added!");
+    }
+
+    public static void viewPatients(){
+        for (Patient p: DataStore.patients){
+            System.out.println(p);
+        }
+    }
+
+    public static void viewRecords(){
+        for (VaccinationRecord r: 
+             DataStore.records) {
+
+            System.out.println(r);
+        }
+    }      
 }
