58cdd8df166386a8c30e6cb06986caac95131b36
SammanthaDube <carlasammantha@gmail.com>
2026-04-30 16:22:07 +0200

    Update CovidVaccine.java

--- CovidVaccine.java
+++ CovidVaccine.java
@@ -1,12 +1,12 @@
 package csi142.model;
 
-public class CovidVaccine extends Vaccine {
+public class CovidVaccine extends Vaccine{
     
-    public CovidVaccine(String Vname, String vaccineID, double doseRequired, int doseIntevarls){
-        super(Vname, vaccineID, doseRequired, doseIntevarls);
+    public CovidVaccine(String vName, String vaccineID, int doseRequired, int doseIntervals){
+        super(vName, vaccineID, doseRequired, doseIntervals);
     }
 
-    @override 
+    @Override 
     public boolean safeForAge(int age) {
         return age >= 12;
     }
