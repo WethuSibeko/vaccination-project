939e491ce8ed4eb12e76aaa9b1bbd6ffbcb244b0
TubukoMabuza <202504344@ub.ac.bw>
2026-04-30 16:58:03 +0200

    Update VaccinationRecord.java

--- VaccinationRecord.java
+++ VaccinationRecord.java
@@ -1,23 +1,29 @@
 package csi142.model;
 
-public class VaccinationRecord{
-private int patientId;
-private String vaccineName;
-private String date;
-  public void VaccinationRecord(int patientId, String vaccineName, String date){
-    this.patientId=patientId;
-    this.vaccineName=vaccineName;
-    this.date=date;
-  }   
-    public int getPatientId(){
-      return patientId
+public class VaccinationRecord {
+    private String patientId;
+    private String vaccineName;
+    private String date;
+
+    public VaccinationRecord(String patientId, String vaccineName, String date) {
+        this.patientId = patientId;
+        this.vaccineName = vaccineName;
+        this.date = date;
+    }
+    public String getPatientId(){
+      return patientId;
         }
     public String getVaccineName(){
-      return vaccineName
+      return vaccineName;
         }
     public String getDate(){
-      return date
+      return date;
         }
-  public String detatils(){
-    System.out.println(patientId+" "+ vaccineName+ " "+ date);
-  }
+
+    @Override
+    public String toString() {
+        return "Patient ID: " + patientId +
+               ", Vaccine: " + vaccineName +
+               ", Date: " + date;
+    }
+}
