934d0343e3f476a42050446da9368d3888715399
WethuSibeko <sibekowethu5@gmail.com>
2026-04-30 16:18:26 +0200

    Update DataStore.java

--- DataStore.java
+++ DataStore.java
@@ -2,8 +2,27 @@ package csi142.data;
 
 import java.util.ArrayList;
 import java.util.List;
+import csi142.model.Patient;
+import csi142.model.VaccinationRecord;
 
 public class DataStore {
-    public static List<Patient> patients = new ArrayList<Patient>();
-    public static List<VaccinationRecord> records = new ArrayList<VaccinatioRecord>();
+
+    private static List<Patient> patients = new ArrayList<Patient>();
+    private static List<VaccinationRecord> records = new ArrayList<VaccinationRecord>();
+
+    public static void addPatient(Patient patient) {
+        patients.add(patient);
+    }
+
+    public static List<Patient> getPatients() {
+        return patients;
+    }
+
+    public static void addRecord(VaccinationRecord record) {
+        records.add(record);
+    }
+
+    public static List<VaccinationRecord> getRecords() {
+        return records;
+    }
 }
